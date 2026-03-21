a22f9d07a3d90309b5a4abb03f5e5b8ab2c9f2d8
Kunal Katiyar <katiyarkunal04@gmail.com>
2026-03-22 01:14:45 +0530

    no double operator

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const display=document.getElementById("display");
- function appendToDisplay(input){
-    display.value += input;
- }
+const display = document.getElementById("display");
+const operator = ['+', '-', '*', '/', '%'];
+function appendToDisplay(input) {
+   const lastDisplay = display.value.slice(-1);
+   if (operator.includes(lastDisplay) && operator.includes(input)) {
+      display.value = display.value.slice(0, -1) + input;
+   }
+   else {
+      display.value += input;
+   }
+}
 
- function clearDisplay(){
-    display.value = "";
- }
+function clearDisplay() {
+   display.value = "";
+}
 
- function backspace(){
-    display.value=display.value.slice(0,-1);
- }
+function backspace() {
+   display.value = display.value.slice(0, -1);
+}
 
- function calculate(){
-    try{
-    display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
- }
+function calculate() {
+   try {
+      display.value = eval(display.value);
+   }
+   catch (error) {
+      display.value = "Error";
+   }
+}
